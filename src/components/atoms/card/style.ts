34b90e84fe1f22c1fc3f10f5da627811431d71aa
Angelo Santos <angelosds@gmail.com>
2022-03-22 11:27:21 -0300

    Removed standardizedPadding style util

--- src/components/atoms/card/style.ts
+++ src/components/atoms/card/style.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
 
-import standardizedPadding from '../../../styles/standardized-padding'
-
 export const Wapper = styled.div`
-  ${standardizedPadding}
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: ${({ theme }) => theme.radius.sm};
   box-shadow: 0 1px 1px 0 rgb(0 0 0 / 10%);
