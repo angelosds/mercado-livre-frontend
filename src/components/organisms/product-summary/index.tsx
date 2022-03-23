3dcc50e5aeb11b55cdc3f8709bdd56454aa9cdc4
Angelo Santos <angelosds@gmail.com>
2022-03-23 15:27:07 -0300

    Added free shiping title to ProductSummary component

--- src/components/organisms/product-summary/index.tsx
+++ src/components/organisms/product-summary/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
 
 import * as S from './style'
 
@@ -11,31 +12,35 @@ export type Props = {
   product: Product
 }
 
-const ProductSummary = ({ product: { free_shipping, id, picture, state_name, price, title } }: Props) => (
-  <Link href={'/items/[id]'} as={`/items/${id}`}>
-    <a>
-      <S.Wrapper>
-        <S.ImageWrapper>
-          <Image src={picture} alt={title} layout="fixed" objectFit="contain" height={180} width={180} priority={true} />
-        </S.ImageWrapper>
-        <S.Container>
-          <S.InfoContainer>
-            <S.Price data-testid="price">
-              $ {price.toLocaleString()}
-              {free_shipping ?
-                <S.TruckIcon data-testid="truck">
-                  <Truck height={12} width={12} />
-                </S.TruckIcon>
-                : null
-              }
-            </S.Price>
-            <S.Place>{state_name}</S.Place>
-          </S.InfoContainer>
-          <S.Title>{title}</S.Title>
-        </S.Container>
-      </S.Wrapper>
-    </a>
-  </Link>
-)
+const ProductSummary = ({ product: { free_shipping, id, picture, state_name, price, title } }: Props) => {
+  const { t } = useTranslation('common')
+
+  return (
+    <Link href={'/items/[id]'} as={`/items/${id}`}>
+      <a>
+        <S.Wrapper>
+          <S.ImageWrapper>
+            <Image src={picture} alt={title} layout="fixed" objectFit="contain" height={180} width={180} priority={true} />
+          </S.ImageWrapper>
+          <S.Container>
+            <S.InfoContainer>
+              <S.Price data-testid="price">
+                {t('currencySymbol')} {price.toLocaleString()}
+                {free_shipping ?
+                  <S.TruckIcon data-testid="truck" title={t('freeShipping')}>
+                    <Truck height={12} width={12} />
+                  </S.TruckIcon>
+                  : null
+                }
+              </S.Price>
+              <S.Place>{state_name}</S.Place>
+            </S.InfoContainer>
+            <S.Title>{title}</S.Title>
+          </S.Container>
+        </S.Wrapper>
+      </a>
+    </Link>
+  )
+}
 
 export default ProductSummary
