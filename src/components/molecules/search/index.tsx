e4d54edad95c2c4e649e63e4d879ec74ae8b9fdb
Angelo Santos <angelosds@gmail.com>
2022-03-23 14:54:16 -0300

    Added placeholder text to search input

--- src/components/molecules/search/index.tsx
+++ src/components/molecules/search/index.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useState } from 'react'
+import { useTranslation } from 'next-i18next'
 
 import * as S from './style'
 
@@ -11,6 +12,7 @@ export type Props = {
 
 const Search = ({ onSubmit }: Props) => {
   const [query, setQuery] = useState('')
+  const { t } = useTranslation('common')
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
@@ -22,7 +24,7 @@ const Search = ({ onSubmit }: Props) => {
 
   return (
     <S.Form onSubmit={handleSubmit}>
-      <Input radius='left' type="search" value={query} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)} />
+      <Input radius="left" placeholder={t('searchPlaceholder')} type="search" value={query} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)} />
       <S.Submit type="submit">
         <SearchIcon height={18} width={18} />
       </S.Submit>
